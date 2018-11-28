bdbdb3844aebf51cdeca523e3e7b9a0da0aaaff3
Filip Gajtanovski <f.gajtanovski@gmail.com>
2018-11-29 00:30:51 +0200

    Publisher and Consumer

---
+++ Kafka.Consumer/Consumer.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Confluent.Kafka;
+using Confluent.Kafka.Serialization;
+
+namespace Kafka.Consumer
+{
+    class Consumer
+    {
+        static void Main(string[] args)
+        {
+            var config = new Dictionary<string, object> {
+                { "group.id","thoughts-consumer"},
+                { "bootstrap.servers","localhost:9092"},
+                { "enable.auto.commit", "false"}
+            };
+
+            using (var consumer = new Consumer<Null, string>(config, null, new StringDeserializer(encoding: Encoding.UTF8)))
+            {
+                consumer.Subscribe(new string[] { "philosophize-this" });
+
+                consumer.OnMessage += (_, msg) =>
+                {
+                    Console.WriteLine($"Topic: {msg.Topic} Partion: {msg.Partition} Offset: {msg.Offset} {msg.Value}");
+                    consumer.CommitAsync(msg);
+                };
+
+                while (true)
+                {
+                    consumer.Poll(100);
+                }
+            }
+        }
+    }
+}
---
+++ Kafka.Producer/Producer.cs
@@ -0,0 +1,31 @@
+﻿using Confluent.Kafka;
+using Confluent.Kafka.Serialization;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Kafka.Producer
+{
+    class Producer
+    {
+        static void Main(string[] args)
+        {
+            var config = new Dictionary<string, object>{
+                { "bootstrap.servers","localhost:9092"}
+            };
+
+            using (var producer = new Producer<Null, string>(config, null, new StringSerializer(encoding: Encoding.UTF8)))
+            {
+                string text = null;
+
+                while (text != "exit")
+                {
+                    text = Console.ReadLine();
+                    producer.ProduceAsync("philosophize-this", null, text);
+                }
+
+                producer.Flush(100);
+            }
+        }
+    }
+}
